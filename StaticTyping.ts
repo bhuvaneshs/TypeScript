8ed2b4b8ec83be8246e5837286f61efe3d934fb2
Bhuvanesh <blazingbhuvan@gmail.com>
2017-07-31 16:15:53 +0530

    Add files via upload
    Static Datatypes in typescript

---
+++ StaticTyping.ts
@@ -0,0 +1,15 @@
+let value:number = 1;    // Number (or) decimal (or) hex (or) binary (or) octal datatype 
+let anything:any = "Typescript";  // dynamic datatype
+let isValue:boolean = true   // Boolean datatype
+let objvalue:Object = 4;   // Object datatype
+let undefinedvalue : undefined = undefined;  // undefined type
+let nullvalue:null = null;  // null type
+anything = 2;
+
+
+console.log(value);
+console.log(anything);
+console.log(isValue);
+console.log(objvalue.toString());
+console.log(undefinedvalue);
+console.log(nullvalue);
