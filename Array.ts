fa53e982384b32ae7ebe9299ceb889b2dafd5ffa
Bhuvanesh <blazingbhuvan@gmail.com>
2017-08-01 18:02:09 +0530

    Add files via upload
    Arrays in typescript

---
+++ Array.ts
@@ -0,0 +1,11 @@
+// Array's can be defined in 2 types.
+
+let list:number[] = [1,2,3];
+let listofstring:string[] = ['john','kim','Jean'];
+let arraynumberlist:Array<number> = [10,11,12];
+let arraystringlist:Array<string> = ['hello','hi','hola'];
+let arrayanytypelist:Array<any> = ['Hello',10,'Hi',20];
+
+console.log(arraynumberlist);
+console.log(arraystringlist);
+console.log(arrayanytypelist);
