2d421308f66b3333943f5ee5f50e5cac91b05a15
Bhuvanesh <blazingbhuvan@gmail.com>
2017-08-01 17:05:51 +0530

    Add files via upload
    String templates

---
+++ String.ts
@@ -0,0 +1,5 @@
+let fullname:string = 'Kevin Peterson';
+let age:number =37;
+let details:any = `Full name is ${fullname} and his age is ${age}`;  // $ {} denotes template strings which span multiple lines and embedded                                                                               expressions that surrounded by backtick/backquote (``)
+
+console.log(details);
