17fa556ba585575cb143ab21fde4a8db709fa80f
Bhuvanesh <blazingbhuvan@gmail.com>
2017-08-01 18:14:15 +0530

    Add files via upload
    Generic array type and loop through array object.

--- loopthrougharray.ts
+++ loopthrougharray.ts
@@ -1,6 +1,6 @@
-let list:Array<any> = ["hello",'hi',20,'hola'];
+let list:Array<any> = ["hello",'hi',20,'hola'];  // Array<Elemtype> is a generic array type.
 
-for(let mylist of list)  // Syntax for looping through the array in typescript
+for(let mylist of list)  // Syntax for looping through the array in typescript.
 {
    console.log(mylist);
 }
